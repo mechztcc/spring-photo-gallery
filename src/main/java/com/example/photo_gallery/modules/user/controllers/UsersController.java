86dfdbde14e22d88cfc251809aa9a1d1f9020282
mechztcc <dojathan@gmail.com>
2025-09-29 15:04:40 -0300

    feat: add user module

--- src/main/java/com/example/photo_gallery/modules/user/controllers/UsersController.java
+++ src/main/java/com/example/photo_gallery/modules/user/controllers/UsersController.java
@@ -2,6 +2,11 @@ package com.example.photo_gallery.modules.user.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.photo_gallery.modules.user.dto.UserDTO;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +17,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UsersController {
 
     @PostMapping("/create")
-    public String onSave(@RequestBody String name) {
-        return name;
-    }
-
-    @GetMapping()
-    public String onUpdate() {
-        return "onUpdate works";
+    public UserDTO onSave(@Valid @RequestBody UserDTO user) {
+        return user;
     }
 
 }
